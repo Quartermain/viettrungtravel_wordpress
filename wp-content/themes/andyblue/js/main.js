1f3b8aef38762fbd3d7eca5115adccb96eea7f85
trungnguyenthanh <lEt'sgO!@.(none)>
2011-08-18 23:11:51 +0700

    edit mains js update

--- wp-content/themes/andyblue/js/main.js
+++ wp-content/themes/andyblue/js/main.js
@@ -1,9 +1,14 @@
 jQuery(document).ready(function(){	
-		jQuery(".jCarouselLite").jCarouselLite({
+		var counts=jQuery('.jCarouselLite li').length;
+		
+		if (counts>4) {
+				jQuery(".jCarouselLite").jCarouselLite({
 			vertical: true,
 			   auto: true
 
 		});
+		}
+
 		jQuery(".Boxs ul ul li,.menumenunavigation ul ul > li ").hover(function(){jQuery(this).find('ul:first').fadeIn(200)},function(){jQuery(this).find('ul:first').fadeOut(100)});
 		var count=jQuery('.dvtx li').length;
 		if (count>6) {
